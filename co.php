a2bac8583bae2524b495612622a6784427ae3e1a
sl3010 <60118654+sl3010@users.noreply.github.com>
2020-01-21 00:44:12 +0100

    Update co.php

--- co.php
+++ co.php
@@ -5,8 +5,8 @@ error_reporting(-1);
 $mssqldriver = '{SQL Server}';
 $hostname='172.16.3.101';
 $dbname='prace';
-$username='sa';
-$password='Wapro3000';
+$username=''; // usuniete
+$password=''; // usuniete
 $pdo = new PDO("odbc:Driver=$mssqldriver;Server=$hostname;Database=$dbname", $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
